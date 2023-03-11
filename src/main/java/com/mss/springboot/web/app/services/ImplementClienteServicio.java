7b96e321a10935c8a6b88f9172ac423f3eb4fc50
Mike <80673026+mssj-11@users.noreply.github.com>
2023-03-10 22:52:54 -0600

    ImplementClienteServicio
    Esta clase implementa los metodos de la interfaz "IClienteServicio" y los metodos propios de la clase "JpaRepository" de JPA

---
+++ src/main/java/com/mss/springboot/web/app/services/ImplementClienteServicio.java
@@ -0,0 +1,50 @@
+package com.mss.springboot.web.app.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mss.springboot.web.app.models.Cliente;
+import com.mss.springboot.web.app.repositories.ClienteRepositorio;
+
+
+//	Añadiendo los Metodos de la interfaz IClienteServicio
+public class ImplementClienteServicio implements IClienteServicio{
+	
+	//	Inyeccion de Dependencia
+	@Autowired
+	ClienteRepositorio clienteRepositorio;
+	
+	
+	
+	//	Metodos propios de la clase "JpaRepository"
+	@Override
+	public List<Cliente> obtenerTodo() {
+		// TODO Auto-generated method stub
+		return clienteRepositorio.findAll();	//	Devolviendo todo en la tabla Clientes
+	}
+	
+	
+	@Override
+	public Cliente guardar(Cliente cliente) {
+		// TODO Auto-generated method stub
+		return clienteRepositorio.save(cliente);
+	}
+	
+	
+	@Override
+	public Cliente obtenerPorId(long id) {
+		// TODO Auto-generated method stub
+		return clienteRepositorio.findById(id).orElse(null);
+	}
+	
+	
+	@Override
+	public void eliminar(long id) {
+		// TODO Auto-generated method stub
+		clienteRepositorio.deleteById(id);
+	}
+	
+	
+	
+}
