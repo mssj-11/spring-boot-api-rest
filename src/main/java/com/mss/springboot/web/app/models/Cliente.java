74a28be6fef0669d915dc1e728cec25cd84a608f
Mike <80673026+mssj-11@users.noreply.github.com>
2023-03-10 22:17:13 -0600

    Clase Cliente

--- src/main/java/com/mss/springboot/web/app/models/Cliente.java
+++ src/main/java/com/mss/springboot/web/app/models/Cliente.java
@@ -2,17 +2,29 @@ package com.mss.springboot.web.app.models;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
 //	Implementando la interfaz Serializable
 public class Cliente implements Serializable{
+	
 	
 	//	Variable 
 	private static final long serialVersionUID = 1L;
 	
+	
 	//	Atributos
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String nombre;
 	private String apellidos;
 	private String email;
+	
 	
 	
 	//	Constructor vacio
@@ -22,42 +34,26 @@ public class Cliente implements Serializable{
 
 	
 	//	Getter and Setter
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-
-
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-
-	public String getApellidos() {
-		return apellidos;
-	}
-	public void setApellidos(String apellidos) {
-		this.apellidos = apellidos;
-	}
-
-
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-
+	public long getId() {	return id;	}
+	public void setId(long id) {	this.id = id;	}
+	
+	
+	public String getNombre() {	return nombre;	}
+	public void setNombre(String nombre) {	this.nombre = nombre;	}
+	
+	
+	public String getApellidos() {	return apellidos;	}
+	public void setApellidos(String apellidos) {	this.apellidos = apellidos;		}
+	
+	
+	public String getEmail() {	return email;	}
+	public void setEmail(String email) {	this.email = email;		}
+	
+	
+	
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-	
 	
 	
 	
